0313cf0af82da9daf4989889a79a299db5f27b14
Olivia Toolson <livytoolson@gmail.com>
2020-12-15 12:24:11 -0800

    MVP

--- api/server.js
+++ api/server.js
@@ -1,33 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const session = require('express-session');
-
-const KnexSessionStore = require('connect-session-knex')(session)
+const session = require('session');
 
 const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
+const config = require('./cookieConfig');
 
 const server = express();
-
-const config = {
-    name: 'banana',
-    secret: 'Keep it secret, keep it safe!',
-    cookie: {
-        maxAge: 1000 * 60 * 60,
-        secure: false,
-        httpOnly: true,
-    },
-    resave: false,
-    saveUninitialized: false,
-    store: new KnexSessionStore({
-        knex: require('../data/dbConfig'),
-        tablename: 'sessions',
-        sidfieldname: 'sid',
-        createtable: true,
-        clearInterval: 1000 * 60 * 60,
-    })
-};
 
 server.use(cors());
 server.use(helmet());
